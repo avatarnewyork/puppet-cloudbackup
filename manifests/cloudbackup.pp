8e898ae460e496e3c01acfdfd524685a0e8698e6
patrick tully <patrick@avatarnewyork.com>
2012-09-03 16:01:19 -0400

    Added files for testing build via Travis CI

--- manifests/cloudbackup.pp
+++ manifests/cloudbackup.pp
@@ -10,21 +10,19 @@ class cloudbackup::install {
     baseurl => 'http://agentrepo.drivesrvr.com/redhat/',
     descr => 'drivesrvr'
   }
-  
   package {'driveclient':
     ensure => latest,
     require => Yumrepo['drivesrvr'],
     notify => Exec['driveclient --configure'],
   }
-
-  # Not sure why, but it seems to always ask for permission to overwrite the bootstrap.json file first
-  # added a 'Y' to the parameters below
+  # Not sure why, but it seems to always ask for
+  # permission to overwrite the bootstrap.json file
+  # first added a 'Y' to the parameters below
   exec {'driveclient --configure':
     command => "/usr/bin/printf '%s\n' Y ${cloudbackup::username} ${cloudbackup::api_key} | /usr/local/bin/driveclient --configure",
     refreshonly => true,
     require => Package['driveclient']
   }
-  
 }
 
 class cloudbackup::service {
