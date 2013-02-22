d75aada111b9797233847824cf7f258bf7ab1245
Dave Zielinski <davez@avatarnewyork.com>
2013-02-22 13:14:16 -0500

    Refresh with subscribe instead of notify

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,15 +8,14 @@ class cloudbackup::install {
   package {'driveclient':
     ensure  => latest,
     require => Yumrepo['drivesrvr'],
-    notify  => Exec['driveclient --configure'],
   }
   # Not sure why, but it seems to always ask for
   # permission to overwrite the bootstrap.json
   # file first added a 'Y' to the parameters below
   exec {'driveclient --configure':
     command     => "/usr/bin/printf '%s\n' Y ${cloudbackup::username} ${cloudbackup::api_key} | /usr/local/bin/driveclient --configure",
-    refreshonly => true,
-		unless			=> "/usr/bin/test -f /etc/driveclient/bootstrap.json",
-    require     => Package['driveclient']
+    unless			=> "/usr/bin/test -f /etc/driveclient/bootstrap.json",
+		subscribe		=> Package['driveclient'],
+		refreshonly => true,		
   }
 }
